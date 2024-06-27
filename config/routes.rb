d347ec9dc5688e9bf2bd2d014210405d2c065c90
rajthakkar02 <rajthakkar0302@gmail.com>
2024-06-27 16:49:50 +0530

    fix: some issue regarding error

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,26 @@
 Rails.application.routes.draw do
-  resources :cart_items
+  resources :cart_items , only: %i[create show destroy ]
   resource :cart, only: %i[ show destroy create ] do
-    post "add/:book_id", to: "carts#add", as: "add_to"
-    post "/orders", to: "orders#create"
+    post "add/:book_id", to: "carts#add", as: "add_to_cart"
     delete "destroy/:cart_id", to: "cart_items#destroy"
-    patch "increase/:cart_item_id", to: "carts#increase", as: "increase"
-    patch "decrease/:cart_item_id", to: "carts#decrease", as: "decrease"
+    patch "increase/:cart_item_id", to: "carts#increase", as: "increase_cart_value"
+    patch "decrease/:cart_item_id", to: "carts#decrease", as: "decrease_cart_value"
   end
   resources :books do
-    resources :feedbacks, only: [:create, :edit, :update, :destroy]
+    resources :feedbacks, only: %i[show]
     get "books/:book_id", to: "books#show"
-    get "/books/:book_id/feedbacks/:id/edit", to: "books/feedbacks#edit"
   end
-  resources :authors do
-    resources :feedbacks, only: [:create, :edit, :update, :destroy]
-  end
-  resources :orders do
-    member do
-      patch :cancel
-    end
-  end
-  resources :authors
-  devise_for :users
+  # resources :authors do
+  #   resources :feedbacks, only: [:create, :edit, :update, :destroy]
+  # end
+  # resources :orders do
+  #   member do
+  #     patch :cancel
+  #   end
+  # end
+  # resources :authors
 
+  devise_for :users
   get "/register_seller", to: "users#new_seller"
   post "/register_seller", to: "users#create_seller"
   ActiveAdmin.routes(self)
@@ -34,12 +32,40 @@ Rails.application.routes.draw do
   root "home#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # authenticate :user, ->(u) { !u.seller? } do
-  #   root 'books#index'
-  # end
-  # Defines the root path route ("/")
-  # root "posts#index"
+  authenticate :user, ->(u) { !u.seller? } do
+    resources :cart_items, only: %i[create show destroy]
+    resource :cart, only: %i[ show destroy create ] do
+      post "add/:book_id", to: "carts#add", as: "add_to"
+      post "/orders", to: "orders#create"
+      delete "destroy/:cart_id", to: "cart_items#destroy"
+      patch "increase/:cart_item_id", to: "carts#increase", as: "increase"
+      patch "decrease/:cart_item_id", to: "carts#decrease", as: "decrease"
+    end
+    resources :books, only: %i[show] do
+      resources :feedbacks, only: [:create, :edit, :update, :destroy]
+      get "books/:book_id", to: "books#show"
+      get "/books/:book_id/feedbacks/:id/edit", to: "books/feedbacks#edit"
+    end
+    resources :authors, only: %i[show] do
+      resources :feedbacks, only: [:create, :edit, :update, :destroy]
+    end
+    resources :orders, only: %i[index create] do
+      member do
+        patch :cancel
+      end
+    end
+  end
+
+  authenticate :user, ->(u) { u.seller? } do
+    resources :books do
+      resources :feedbacks, except: %i[show]
+    end
+    resources :authors do
+      resources :feedbacks, except: %i[show]
+    end
+    resources :orders
+  end
   match "*path", via: :all, to: "application#not_found_method", constraints: lambda { |req|
-    req.path.exclude? "rails/active_storage"
+             req.path.exclude? "rails/active_storage"
   }
 end
